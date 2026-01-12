c72fd90d5a6dd529bbe56926d952e421ee7cb39c
Anitonchik <anitonchikantonova@yandex.ru>
2026-01-12 19:17:34 +0400

    initial

---
+++ src/main/java/com/example/TravelAgencyServer/TravelAgencyServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.TravelAgencyServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAgencyServerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/TravelAgencyServer/entity/ClientEntity.java
@@ -0,0 +1,4 @@
+package com.example.TravelAgencyServer.entity;
+
+public class ClientEntity {
+}
